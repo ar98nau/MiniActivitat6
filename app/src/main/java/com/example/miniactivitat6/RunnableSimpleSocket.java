7ba4c60658baf794c40543760a86e01f55f69564
ar98nau <anr3@alumnes.udl.cat>
2020-04-11 21:34:38 +0200

    SimpleSocket handle activities
    Signed-off-by: ar98nau <anr3@alumnes.udl.cat>

--- app/src/main/java/com/example/miniactivitat6/RunnableSimpleSocket.java
+++ app/src/main/java/com/example/miniactivitat6/RunnableSimpleSocket.java
@@ -1,17 +1,16 @@
 package com.example.miniactivitat6;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -22,9 +21,9 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class SimpleSocket extends AppCompatActivity {
+public class RunnableSimpleSocket extends AppCompatActivity {
 
-    private static final String CLASSTAG = SimpleSocket.class.getSimpleName();
+    private static final String CLASSTAG = RunnableSimpleSocket.class.getSimpleName();
 
     private EditText ipAddress;
     private EditText port;
@@ -47,17 +46,7 @@ public class SimpleSocket extends AppCompatActivity {
         socketButton = findViewById(R.id.socket_button);
 
         //StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
-        this.h = new Handler() {
-            @Override
-            public void handleMessage(@NonNull Message msg) {
-                switch (msg.what) {
-                    case 0:
-                        socketOutput.setText((String) msg.obj);
-                        break;
-                }
-                super.handleMessage(msg);
-            }
-        };
+        this.h = new Handler();
 
         socketButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,10 +59,7 @@ public class SimpleSocket extends AppCompatActivity {
                             try {
                                 InetAddress serverAddr = InetAddress.getByName(ipAddress.getText().toString());
                                 String output = callSocket(serverAddr, port.getText().toString(), socketInput.getText().toString());
-                                Message msg = new Message();
-                                msg.what = 0;
-                                msg.obj = output;
-                                SimpleSocket.this.h.sendMessage(msg);
+                                RunnableSimpleSocket.this.h.post(new updateSocketOutput(output));
                             } catch (UnknownHostException e) {
                                 e.printStackTrace();
                             }
@@ -104,13 +90,13 @@ public class SimpleSocket extends AppCompatActivity {
             writer.flush();
 
             output = reader.readLine();
-            Log.d("NetworkExplorer", " " + SimpleSocket.CLASSTAG + " output - " + output);
+            Log.d("NetworkExplorer", " " + RunnableSimpleSocket.CLASSTAG + " output - " + output);
 
             writer.write("EXIT\n", 0, 5);
             writer.flush();
 
         } catch (IOException e) {
-            Log.d("NetworkExplorer", " " + SimpleSocket.CLASSTAG + " IOException calling socket", e);
+            Log.d("NetworkExplorer", " " + RunnableSimpleSocket.CLASSTAG + " IOException calling socket", e);
         } finally {
             try {
                 if (writer != null)
@@ -129,5 +115,18 @@ public class SimpleSocket extends AppCompatActivity {
             }
         }
         return output;
+    }
+
+    private class updateSocketOutput implements Runnable {
+        String output;
+
+        public updateSocketOutput(String output) {
+            this.output = output;
+        }
+
+        @Override
+        public void run() {
+            socketOutput.setText(output);
+        }
     }
 }
